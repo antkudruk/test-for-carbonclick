9750802b9ef140548db76e10531b7bb56c7eb920
anton <antkudruk@gmail.com>
2020-08-18 22:03:01 +0300

    Unblock secret santa

--- src/main/java/com/carbonclick/tsttask/secretsanta/assignment/service/AssignmentService.java
+++ src/main/java/com/carbonclick/tsttask/secretsanta/assignment/service/AssignmentService.java
@@ -8,16 +8,19 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class AssignmentService {
     public List<AssignmentDto> assign(Set<Long> participants) {
+        Set<Long> giversSet = new HashSet<>(participants);
         Set<Long> takersSet = new HashSet<>(participants);
 
         return participants.stream()
                 .map(giver -> {
-                    long taker = getAnotherRandom(giver, takersSet);
+                    long taker = getSafeAnotherRandom(giver, takersSet, giversSet);
                     takersSet.remove(taker);
+                    giversSet.remove(giver);
                     return AssignmentDto.builder()
                             .giverId(giver)
                             .takerId(taker)
@@ -27,11 +30,30 @@ public class AssignmentService {
     }
 
     // If just two participants remained, giver can't be equal to taker for a one participant
-    private long getSafeAnotherRandom(long giver, Set<Long> takers) {
+    private long getSafeAnotherRandom(long giver, Set<Long> takers, Set<Long> givers) {
         if(takers.size() == 2) {
+            Stream<Long> takerNotInGivers = takers.stream()
+                    .filter(t -> !givers.contains(t));
+
+            if(takerNotInGivers.count() == 1) {
+                return takerNotInGivers.findFirst().orElseThrow(RuntimeException::new);
+            } else {
+                return getAnotherRandom(giver, takers);
+            }
+            /*
+            Stream<Long> giverTakerIntersection = takers.stream().filter(givers::contains);
+            if(giverTakerIntersection.count() == 1) {
+                Long notInvolved = giverTakerIntersection
+                        .findFirst()
+                        .orElseThrow(RuntimeException::new);
+                return takers.stream()
+                        .filter(t -> t != notInvolved)
+                        .findFirst()
+                        .orElseThrow(RuntimeException::new);
+            }*/
             // TODO:
         } else {
-            getAnotherRandom(giver, takers);
+            return getAnotherRandom(giver, takers);
         }
     }
 
