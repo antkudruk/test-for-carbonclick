ef83d58405a888f744dd48ec99b38e163e818fb3
anton <antkudruk@gmail.com>
2020-08-24 23:59:51 +0300

    Refactored test

--- src/test/java/com/carbonclick/tsttask/secretsanta/assignment/controller/YearControllerTest.java
+++ src/test/java/com/carbonclick/tsttask/secretsanta/assignment/controller/YearControllerTest.java
@@ -1,5 +1,6 @@
 package com.carbonclick.tsttask.secretsanta.assignment.controller;
 
+import com.carbonclick.tsttask.secretsanta.BaseTest;
 import com.carbonclick.tsttask.secretsanta.assignment.controller.request.NewAssignmentRequest;
 import com.carbonclick.tsttask.secretsanta.assignment.controller.response.AssignmentResponse;
 import com.carbonclick.tsttask.secretsanta.assignment.controller.response.YearResponse;
@@ -11,10 +12,6 @@ import com.carbonclick.tsttask.secretsanta.base.page.PageRequest;
 import com.carbonclick.tsttask.secretsanta.participant.controller.request.ParticipantRequest;
 import com.carbonclick.tsttask.secretsanta.participant.controller.response.ParticipantResponse;
 import com.carbonclick.tsttask.secretsanta.participant.repository.ParticipantRepository;
-import com.carbonclick.tsttask.secretsanta.user.controller.request.LoginRequest;
-import com.carbonclick.tsttask.secretsanta.user.repository.entity.UserEntity;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,12 +20,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -38,62 +33,16 @@ import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class YearControllerTest {
-
-    @Autowired
-    private MockMvc mvc;
+public class YearControllerTest extends BaseTest {
 
     @Autowired
     private ParticipantRepository participantRepository;
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private EntityManager entityManager;
-
-    @Autowired
     private YearRepository yearRepository;
-
-    private String authToken;
-
-    public static class TestLoginResponse {
-        public String token;
-    }
-
-    @BeforeEach
-    public void initJwtToken() throws Exception {
-        UserEntity user = UserEntity.builder()
-                .username("admin")
-                .password("$2a$10$aDipcD0hx5janQqNYMqKBe.fBx6TSY8Kvyu4zfyQ0oU4/qs4TK/1O")   // 123456
-                .build();
-
-        entityManager.persist(user);
-
-        LoginRequest req = new LoginRequest("admin", "123456");
-
-        String response = mvc.perform( MockMvcRequestBuilders
-                .post("/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .with(csrf())
-                .content(objectMapper.writeValueAsString(req))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-
-        authToken = objectMapper.readValue(response, TestLoginResponse.class).token;
-    }
 
     @Test
     @Transactional
